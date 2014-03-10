096eb3fac8f68bc0cce575496a0f2d6000ed21e2
thomasmmc <thomasmmc@gmail.com>
2014-03-09 22:25:01 -0400

    working and sending emails using send_alert_mail

--- sn_do.rb
+++ sn_do.rb
@@ -9,6 +9,10 @@ instance_name = config["instance_name"]
 @username = config["username"]
 @password = config["password"]
 assign_group_id = config["assign_group_id"]
+assign_group_name = config["assign_group_name"]
+@recipients = config["recipients"].split(",")
+@smtpserver = config["smtpserver"]
+@from = config["fromaddress"]
 
 #set service now instance name
 @sninstance_url = "https://#{instance_name}.service-now.com/"
@@ -76,13 +80,24 @@ def get_incident_details(array, number)
 	results
 end
 
+def filter_incidents(array, type, query_param, query_value)
+	results = array.select do |inc|
+		case type
+		when 'in'
+		inc[query_param] == query_value
+		when 'out'
+		inc[query_param] != query_value
+		end
+	end
+	results
+end
+
 #create our incident list array to capture the data
 incident_list = Array.new
-
 #Running getincidents for the assignment group specified in the yaml
-httpresult = getincidents('XML','assignment_group', assign_group_id)
-xmldoc = Document.new(httpresult.body)
-#xmldoc = Document.new(xmlraw)
+#httpresult = getincidents('XML','assignment_group', assign_group_id)
+#xmldoc = Document.new(httpresult.body)
+xmldoc = Document.new File.new('sample.xml')
 #Now we are going to reiderate over all the XML data and get some info into an array
 xmldoc.elements.each("xml/incident") do |inc|
 	details = Hash.new
@@ -96,15 +111,19 @@ xmldoc.elements.each("xml/incident") do |inc|
 	details[:incident_state] = inc.elements['incident_state'].text
 	details[:updated] = inc.elements['sys_updated_on'].text
 	details[:updated_by] = inc.elements['sys_updated_by'].text
+	details[:u_business_service] = inc.elements['u_business_service'].text
 	#finaly we are loding our hash into our array
 	incident_list << details
 end
 
+incident_list = filter_incidents(incident_list, 'out', :u_business_service, 'ce0f2cdab54a4100948f468985d34378')
 total_incidents = incident_list.count
 openedlast_24 = count_incidents_by_datediff(incident_list,:opened,'less',1)
 olderthen7days = count_incidents_by_datediff(incident_list,:opened,'more',7)
 notupdated7days = count_incidents_by_datediff(incident_list,:updated,'more',7)
 notupdated7days_details = incidentdetails_by_datediff(incident_list,:updated,'more',7)
+#notupdated7days_details = filter_incidents(notupdated7days_details, 'out', :u_business_service, 'ce0f2cdab54a4100948f468985d34378') #Filting out Online Directory
+
 
 #Now we are going to add a url to each hash based on sys_id and instance
 notupdated7days_details.each do |inc|
@@ -112,17 +131,21 @@ notupdated7days_details.each do |inc|
 	inc[:url] = link
 end
 
-puts "Total Incidents in queue #{total_incidents}"
-puts "Total Incidents last day #{openedlast_24}"
-puts "Total Incidents older the 7 days #{olderthen7days}"
-puts "Total Incidents not updated 7 days #{notupdated7days}" 
+#building our the details for the message
 if notupdated7days_details.any?
-	info = "<b>Details on Incidents not updated for 7 days</b>\n"
+	info = "<b>Details on Incidents not updated for 7 days</b>\n<br>"
 	notupdated7days_details.each do |inc|
-		info << %Q^Number: <a href="#{inc[:url]}">#{inc[:number]}</a> Priority: #{inc[:priority]} Updated_by: #{inc[:updated_by]} Desc: #{inc[:short_desc]}\n^
+		info << %Q^Number: <a href="#{inc[:url]}">#{inc[:number]}</a> Priority: #{inc[:priority]} Updated_by: #{inc[:updated_by]} Desc: #{inc[:short_desc]}\n<br>^
 	end
 end
-puts info
-
+@subject = "SN: #{assign_group_name} Daily Status Report"
+header = "<font face=helvetica> This is todays stats for #{assign_group_name}\n<br>"
+stats = "Total Incidents in queue: #{total_incidents}<br>\n"
+stats << "Total Incidents last day: #{openedlast_24}<br>\n"
+stats << "Total Incidents older the 7 days: #{olderthen7days}<br>\n"
+stats << "Total Incidents not updated 7 days: #{notupdated7days}<br>\n"
+footer = %Q^This report is part of alpha testing of sn_do <a href="https://github.com/thomasmmc/sn_do"><br><img src="https://github.global.ssl.fastly.net/images/modules/logos_page/GitHub-Mark.png" width=20 height=20>https://github.com/thomasmmc/sn_do</a></font>^
+@body = "#{header}<br>#{stats}<br>#{info}<br>#{footer}"
+load 'send_alert_mail.rb'
 
 
